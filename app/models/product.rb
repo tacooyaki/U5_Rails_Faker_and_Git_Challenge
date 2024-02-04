c3bf0907875bc39c4577bed0852f6461212a2151
tacooyaki <p_mercredi@yahoo.ca>
2024-02-04 15:49:30 -0600

    Removed data from the products table. Migrated to add FK refrence from product model to a category. Added belongs_to and has_many associations to category and product models.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
   validates :stock_quantity, presence: true
+
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_04_212544) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_214355) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_04_212544) do
     t.integer "stock_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
