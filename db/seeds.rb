876636d402f544fb6280c0dca0e5f211f99c9a4e
tacooyaki <p_mercredi@yahoo.ca>
2024-02-04 16:04:50 -0600

    Rewrote seeds.rb to pull product/category data from products.csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price(range: 0.01..1000.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+require "csv"
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db', 'products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+
+  category.products.create(
+    title: product['name'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock_quantity']
   )
 end
+
+puts "Imported #{Category.count} categories and #{Product.count} products."
