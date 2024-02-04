60b20066305661609914b36ebfca8833ee429910
tacooyaki <p_mercredi@yahoo.ca>
2024-02-04 15:28:32 -0600

    New model categorty created. Has string name included

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'products#index'
   get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: 'product'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_040745) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_212544) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
